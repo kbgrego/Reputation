645d0e36ca96be9dc19d412136b3c1f6f6ec20a1
Gregory Kabanov <kabanov@edsson.com>
2018-08-01 14:58:21 +0300

    Добавлен токен авторизации
    при авторизации генерируется временный токен для запросов к сайту.

--- plugin/reputation_rpc.php
+++ plugin/reputation_rpc.php
@@ -14,53 +14,60 @@ if ( ! defined( 'ABSPATH' ) ) { // prevent full path disclosure
 	exit;
 }
 
-function reputation_getUserID( $args ) {
-    global $wp_xmlrpc_server;
-    $wp_xmlrpc_server->escape( $args );
-
-    $username = $args[0];
-    $password = $args[1];
-
-    if ( ! $user = $wp_xmlrpc_server->login( $username, $password ) )
-        return $wp_xmlrpc_server->error;
-
-    return $user->ID;    
-}
-
 function reputation_getAuth( $args ) {
     global $wp_xmlrpc_server;
     $wp_xmlrpc_server->escape( $args );
 
     $username = $args[0];
     $password = $args[1];
-    $guid = $args[2];
 
     if ( ! $user = $wp_xmlrpc_server->login( $username, $password ) ){        
         $return['auth'] = false;
     } else {
-        $new_key_pair = openssl_pkey_new(array(
-            "private_key_bits" => 2048,
-            "private_key_type" => OPENSSL_KEYTYPE_RSA,
-        ));
-
-        openssl_pkey_export($new_key_pair, $private_key_pem);
-
-        openssl_sign($guid, $signature, $private_key_pem, OPENSSL_ALGO_SHA256);
+        $guid = wp_generate_auth_cookie($user->ID, 120);
 
         $return['auth'] = true;
-        $return['signature'] = base64_encode($signature);
+        $return['token'] = $guid;
     }
-
-
 
     return $return;    
 }
 
+function reputation_checkAuth( $args ) {
+    global $wp_xmlrpc_server;
+    $wp_xmlrpc_server->escape( $args );
+
+    $cookie = $args[0];
+
+    
+    if ( wp_validate_auth_cookie($cookie) ){        
+        $return['auth'] = false;
+    } else {
+        $return['auth'] = true;
+    }
+
+    return $return;    
+}
+
+
+
 function reputation_xmlrpc_methods( $methods ) {
-    $methods['reputation.getUserID'] = 'reputation_getUserID';
     $methods['reputation.getAuth'] = 'reputation_getAuth';
+    $methods['reputation.checkAuth'] = 'reputation_checkAuth';
+
     return $methods;   
 }
 add_filter( 'xmlrpc_methods', 'reputation_xmlrpc_methods');
+
+function guidv4()
+{
+    if (function_exists('com_create_guid') === true)
+        return trim(com_create_guid(), '{}');
+
+    $data = openssl_random_pseudo_bytes(16);
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}
 
 ?>
